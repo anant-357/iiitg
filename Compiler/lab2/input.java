2790ba9dd7b0c2b185fbb58f60df97e5ba930b4c
anant-357 <anantsh.88@gmail.com>
2024-01-25 16:48:51 +0530

    lab2

---
+++ Compiler/lab2/input.java
@@ -0,0 +1,20 @@
+// Your First Program
+
+/**
+ this is documentation code
+ so this should come in doc.txt file
+ */
+
+/**
+   another doc code
+   this will also be in doc.txt
+*/
+
+class HelloWorld {
+    public static void main(String[] args) {
+        /* this will also be removed as it is
+        a comment */
+        System.out.println("Hello, World!"); 
+    }
+}
+
